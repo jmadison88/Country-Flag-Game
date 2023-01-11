f66a63c7c7ea42cde5f773f42c648c96ffd85eac
jmadison <jmad99@sbcglobal.net>
2023-01-11 09:29:57 -0600

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Josh Madison on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
