dc7b14bb3d2f93e148f444191bd186101726fbaa
jmadison <jmad99@sbcglobal.net>
2023-01-11 09:50:21 -0600

    Added Country Flag Images, plus Question and Answer structs

---
+++ Country Flag Game/Models/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Country Flag Game
+//
+//  Created by Josh Madison on 1/11/23.
+//
+
+import Foundation
+
+struct Answer: Identifiable {
+    var id = UUID()
+    var text: String
+    var isCorrect: Bool
+}
+
+struct Question: Identifiable {
+    var id = UUID()
+    var correctAnswer: Answer
+    var incorrectAnswers: [Answer]
+}
